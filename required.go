7f7189b6a61dab6e8ac6171cab3e1a4dbb6a1562
bgibson <bgibson@foreverliving.com>
2015-08-27 17:00:06 -0700

    2nd commit, remove .idea/ cruft

--- required.go
+++ required.go
@@ -1,1 +1,40 @@
 package validate
+
+import (
+	"errors"
+	"reflect"
+)
+
+const (
+	STRING = "string"
+	INT    = "int"
+)
+
+func ValidateRequired(i interface{}) (error, []string) {
+	var err error
+	t := reflect.TypeOf(i)
+	err = errors.New("Error: " + t.Name() + " has required fields")
+	infractions := []string{}
+	ivalue := reflect.ValueOf(i)
+
+	for index := 0; index < t.NumField(); index++ {
+		field := t.Field(index)
+		name := field.Name
+		required := field.Tag.Get("required")
+
+		if required != "" {
+			if field.Type.Name() == STRING {
+				if ivalue.FieldByName(name).String() == "" {
+					infractions = append(infractions, name)
+				}
+			}
+		}
+	}
+
+	if len(infractions) == 0 {
+		err = nil
+		infractions = nil
+	}
+
+	return err, infractions
+}
